a386ed13b4263656114e108215cf35d4b5ab7df2
Jorge Ramirez <jorgeramirezinstituto@gmail.com>
2020-12-11 14:32:10 +0100

    update

--- PhpMailer/execute.php
+++ PhpMailer/execute.php
@@ -1,6 +1,8 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
-require 'vendor/autoload.php';
+require "PHPMailer.php";
+require "Exception.php";
+require "SMTP.php";
 $mail = new PHPMailer;
 $mail->isSMTP();
 $mail->SMTPDebug = 2;
@@ -8,13 +10,13 @@ $mail->SMTPAuth = true;
 $mail->SMTPSecure = "tls";
 $mail->Host = 'smtp.gmail.com';
 $mail->Port = 587;
-$mail->Username = 'Jorge';
+$mail->Username = 'jorgeramirezinstituto@gmail.com';
 $mail->Password = '';
-$mail->setFrom('jorgeramirezinstituto@gmail.com', 'TEST');
+$mail->setFrom('jorgeramirezinstituto@gmail.com', 'Jorge');
 $mail->Subject = 'Correo de Prueba';
 $mail->msgHTML('Prueba');
-// $mail->addAttachment('XXX');
-$address "pedro.cros.profe@gmail.com";
+$mail->addAttachment('tragabuche.jpg');
+$address = "pedro.cros.profe@gmail.com";
 $mail->addAddress ($address,"Test");
 //enviar
 $resul = $mail->send();
